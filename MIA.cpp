c3eb0ccba46eb6051349ed4dd1da896a7c48f636
TheReclif <17992687+TheReclif@users.noreply.github.com>
2022-02-19 19:35:43 +0100

    Proper parameters; file loading utility

--- MIA.cpp
+++ MIA.cpp
@@ -1,14 +1,5 @@
 #include <argumentum/argparse.h>
 
-#include <spdlog/spdlog.h>
-
-#include <cppast/code_generator.hpp>         // for generate_code()
-#include <cppast/cpp_entity_kind.hpp>        // for the cpp_entity_kind definition
-#include <cppast/cpp_forward_declarable.hpp> // for is_definition()
-#include <cppast/cpp_namespace.hpp>          // for cpp_namespace
-#include <cppast/libclang_parser.hpp> // for libclang_parser, libclang_compile_config, cpp_entity,...
-#include <cppast/visitor.hpp>         // for visit()
-
 #include <vector>
 #include <string>
 
@@ -26,8 +17,8 @@ int main(int argc, char** argv)
 
 	parser.config().program(argv[0]).description("Meta Information Appender");
 	params.add_parameter(filesToProcess, "files").minargs(1).metavar("FILES").help("Files to process");
-	params.add_parameter(outputPattern, "--output", "-o").metavar("OUTPUT_PATTERN").help("Output pattern for the output files. Defaults to \"{}.hpp\" where {} is a placeholder for the input file name").required(false).absent("{}.hpp");
-	params.add_parameter(threadCount, "--threads", "-t").metavar("THREADS").help("How many threads to start").required(false).absent(-1);
+	params.add_parameter(outputPattern, "--output", "-o").metavar("OUTPUT_PATTERN").help("Output pattern for the output files. Defaults to \"{}.hpp\" where {} is a placeholder for the input file name").required(false).absent("{}.hpp").nargs(1);
+	params.add_parameter(threadCount, "--threads", "-t").metavar("THREADS").help("How many threads to start").required(false).absent(-1).nargs(1);
 
 	auto res = parser.parse_args(argc, argv);
 	if (!res)
--- src/App.cpp
+++ src/App.cpp
@@ -1,9 +1,18 @@
 #include <App.hpp>
 
+#include <spdlog/spdlog.h>
+
+#include <cppast/code_generator.hpp>         // for generate_code()
+#include <cppast/cpp_entity_kind.hpp>        // for the cpp_entity_kind definition
+#include <cppast/cpp_forward_declarable.hpp> // for is_definition()
+#include <cppast/cpp_namespace.hpp>          // for cpp_namespace
+#include <cppast/libclang_parser.hpp> // for libclang_parser, libclang_compile_config, cpp_entity,...
+#include <cppast/visitor.hpp>         // for visit()
+
 #include <thread>
 #include <fstream>
 
-std::string readWholeFile(const std::string& filename)
+static std::string readWholeFile(const std::string& filename)
 {
 	std::ifstream is;
 	is.open(filename);
@@ -12,17 +21,27 @@ std::string readWholeFile(const std::string& filename)
 		return "";
 	}
 
-	// TODO
+	is.seekg(0, std::ios::end);
+	const auto size = is.tellg();
+	is.seekg(0, std::ios::beg);
+
+	std::string result(size, '\0');
+	is.read(result.data(), size);
+	
+	return result;
 }
 
 void mia::App::threadFunc()
 {
 	const auto fileCount = filesToProcess.size();
 	unsigned int currentFile = currentProcessedFile++;
+
 	while (currentFile < fileCount)
 	{
-		const std::string currentFileName = filesToProcess[currentFile];
+		const std::string fileContents = readWholeFile(filesToProcess[currentFile]);
 		// TODO
+
+		spdlog::info("{}: {} bytes", filesToProcess[currentFile], fileContents.size());
 
 		currentFile = currentProcessedFile++;
 	}
@@ -36,7 +55,7 @@ void mia::App::process()
 {
 	currentProcessedFile = 0u;
 
-	const unsigned int threadsToUse = (threadCount > 0) ? threadCount : std::thread::hardware_concurrency();
+	const unsigned int threadsToUse = (threadCount > 0) ? threadCount : std::min(std::thread::hardware_concurrency(), static_cast<unsigned int>(filesToProcess.size()));
 	std::vector<std::thread> threads;
 	threads.reserve(threadsToUse);
 	for (unsigned int x = 0; x < threadsToUse; ++x)
