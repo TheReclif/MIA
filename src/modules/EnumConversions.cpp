efc6f51f0941084fad5704086efc9aa5d50fd9d1
Michał OsińskiNiikelion <michal.osinski101@gmail.com>
2024-02-29 23:20:07 +0100

    first version of enum to list

--- src/modules/EnumConversions.cpp
+++ src/modules/EnumConversions.cpp
@@ -31,6 +31,16 @@ template<> inline {0} to_enum(const std::string& str) {{
 
 static const auto to_enum_item_pattern = "\t\t{{ {}, {} }}";
 
+static const auto to_list_main_pattern = R"TXT(
+template<> inline std::vector<{0}> enum_to_list(const {0}& v) {{
+	return {{
+{1}
+	}};
+}};
+)TXT";
+
+static const auto to_list_item_pattern = "\t\t{0}";
+
 namespace mia::modules
 {
 	void EnumConversionsModule::extractInfo(std::ostream& outputStream, cppast::cpp_file& source)
@@ -88,7 +98,18 @@ namespace mia::modules
 						[](const EnumOption& option) {
 							return fmt::format(to_enum_item_pattern, option.second, option.first);
 						}
-						), ",\n")
+					), ",\n")
+				);
+
+				outputStream << fmt::format(
+					to_list_main_pattern,
+					eName,
+					text::implode(text::process<EnumOption>(
+						enumOptions,
+						[](const EnumOption& option) {
+							return fmt::format(to_list_item_pattern, option.first);
+						}
+					), ",\n")
 				);
 
 				return true;
